f39800ed112151c20506948330f45e7fd94a8083
ruans <ruansantossacramento32@gmail.com>
2026-06-28 22:29:24 -0300

    feat: implementa auditoria de pagamentos por funcionario com where, order by e limit

--- projects/eighth_project/project8.py
+++ projects/eighth_project/project8.py
@@ -1,19 +1,23 @@
 import mysql.connector
+
 message = []
 count_payments = 0
 count_staff1 = 0
 count_staff2 = 0
+
 connection = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
     passwd = 'Ruan81527733',
     database = 'sakila'
 )
+
 cursor = connection.cursor()
 command_sql = "SELECT staff_id, customer_id, amount FROM payment WHERE amount > %s ORDER BY amount desc LIMIT %s"
 values = (7,15)
 cursor.execute(command_sql, values)
 payments = cursor.fetchall()
+
 for staff_id, customer_id, amount in payments:
     message.append(f'Funcionário {staff_id} | Cliente {customer_id} | R$ {round(amount,2)}\n')
     count_payments += 1
