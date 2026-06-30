6a9887313f5f022204949bf2ebf313a9f415e059
ruans <ruansantossacramento32@gmail.com>
2026-06-30 10:04:41 -0300

    refactor: improve code organization and readability

--- projects/ninth_project/project9.py
+++ projects/ninth_project/project9.py
@@ -1,5 +1,5 @@
 import mysql.connector
-message = []
+report_lines = []
 count_register = 0
 connection = mysql.connector.connect(
     host = 'localhost',
@@ -8,19 +8,31 @@ connection = mysql.connector.connect(
     database = 'sakila'
 )
 cursor = connection.cursor()
-command_sql = "SELECT cus.first_name, cus.last_name, pay.amount, adr.address FROM customer cus JOIN payment pay ON cus.customer_id = pay.customer_id JOIN address adr ON cus.address_id = adr.address_id WHERE amount > %s"
+command_sql = """
+SELECT 
+    cus.first_name,
+    cus.last_name,
+    pay.amount,
+    adr.address 
+FROM customer cus 
+JOIN payment pay  
+    ON cus.customer_id = pay.customer_id 
+JOIN address adr
+    ON cus.address_id = adr.address_id
+WHERE pay.amount > %s
+"""
 values = (7,)
 cursor.execute(command_sql, values)
 reports = cursor.fetchall()
 
-for first_name,last_name, amount, address in reports:
+for first_name, last_name, amount, address in reports:
     full_name = f'{first_name} {last_name}'
-    message.append(f'{full_name} - R$ {amount} - {address}\n')
+    report_lines.append(f'{full_name} - R$ {amount} - {address}\n')
     count_register += 1
-message.append(f'\n{count_register} customers registered')
+report_lines.append(f'\n{count_register} customers registered')
 
-with open('relatorio_receita_clientes.txt', 'w', encoding='utf-8') as r:
-    r.writelines(message)
+with open('relatorio_receita_clientes.txt', 'w', encoding='utf-8') as report_file:
+    report_file.writelines(report_lines)
 
-connection.close()
 cursor.close()
+connection.close()
